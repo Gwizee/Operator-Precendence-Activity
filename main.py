4a7e277c6868a102b028fa55b07c72273d8079a3
Gwizee <risingfire69@gmail.com>
2024-10-21 21:21:40 +0600

    Operator Precendence Activity has been completed

---
+++ main.py
@@ -0,0 +1,15 @@
+a = 4
+b = 5
+c = 2
+d = 8
+
+result = a * b + (d / c)
+print(result)
+
+name = "Alex"
+age = 3
+
+if name == "Alex" or name == "John" and age > 2 :
+    print ("Wlecome !")
+else :
+    print ("Bye !")
